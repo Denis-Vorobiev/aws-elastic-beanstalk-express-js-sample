5bbf348e64e727ceaf57597cc9e7200698a8e1f0
Denis Vorobev <denis.vorobiev@accenture.com>
2021-09-24 16:59:12 +0300

    changed2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi, i wanna be devops'));
+app.get('/', (req, res) => res.send('Hi, i wanna be good devops'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
